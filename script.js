d59b5f1ba85210317004ff8578a58388466104e1
MichoFrost <michofrost@gmail.com>
2023-08-24 10:29:04 +0200

    Console.log elements

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let article1 = document.querySelector(".article1");
+let image1 = document.querySelector(".image1");
+let paragraph1 = document.querySelector(".paragraph1");
+let footerText = document.querySelector(".footerText");
+let subheading = document.querySelector(".subheading");
+// let mainSection = document.querySelector(".mainSection");
+
+// console.log(article1);
+// console.log(image1);
+// console.log(paragraph1);
+// console.log(footerText);
+// console.log(subheading);
+
+
+console.log(document.querySelector(".mainSection").firstChild);
