13db6398a9d070647413269643d9ddff49cbe07d
MichoFrost <michofrost@gmail.com>
2023-08-24 10:45:46 +0200

    textContent changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let article1 = document.querySelector(".article1");
 let image1 = document.querySelector(".image1");
 let paragraph1 = document.querySelector(".paragraph1");
 let footerText = document.querySelector(".footerText");
+let mainHeading= document.querySelector("h1");
 let subheading = document.querySelector(".subheading");
 // let mainSection = document.querySelector(".mainSection");
 
@@ -13,4 +14,13 @@ let subheading = document.querySelector(".subheading");
 // console.log(subheading);
 
 
-console.log(document.querySelector(".mainSection").firstChild);
+// console.log(document.querySelector(".mainSection").firstChild);
+
+
+mainHeading.textContent = "Noget andet!";
+
+let firstArticle = document.querySelector("article:first-child");
+let secondArticle = document.querySelector("article:last-child");
+
+firstArticle.textContent = "Hej med dig, jeg hedder Kaj!";
+secondArticle.textContent = "Save your prayers little one!"
